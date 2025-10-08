ea1be60179ac294a1452048147a32ffc15dfafda
PauloS5 <paulocratesdesouza@gmail.com>
2025-10-07 19:25:27 -0500

    dados gerados pelas execução

---
+++ api/src/test/java/com/sgrs/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgrs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
