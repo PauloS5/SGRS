ce29e468685476c8dc7e8f802e48508d825dd103
PauloS5 <paulocratesdesouza@gmail.com>
2025-10-07 19:37:58 -0500

    Model Room criado

---
+++ api/src/main/java/com/sgrs/api/models/Room.java
@@ -0,0 +1,27 @@
+package com.sgrs.api.models;
+
+public class Room {
+    private Long id;
+    private String name;
+    private String email;
+    
+    public Long getId() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
